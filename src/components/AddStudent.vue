6f0fb692dc7c8849ac82d20efc4b5ca54d1d28df
Michel Domingos <m.domingos76@gmail.com>
2017-05-23 15:05:24 +0200

    add put - delete - post - getall

---
+++ src/components/AddStudent.vue
@@ -0,0 +1,39 @@
+
+
+<script>
+export default {
+  data() {
+    return {
+      inputNewStudent: '',
+    };
+  },
+
+  computed: {
+    addNewStudent() {
+      return {
+        firstname: this.inputNewStudent,
+      };
+    },
+  },
+
+  methods: {
+    addStudent() {
+      this.$emit('addStudent', this.addNewStudent);
+      this.inputNewStudent = '';
+    },
+  },
+};
+</script>
+
+<template >
+<div class="">
+
+  <h1>Add New Student</h1>
+  <input v-model='inputNewStudent'>
+  <button type="button" v-on:click="addStudent()">Add Student</button>
+
+</div>
+</template>
+
+<style>
+</style>
